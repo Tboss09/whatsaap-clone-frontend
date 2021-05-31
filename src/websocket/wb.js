2874c6bfaab73cbc51ee179faaf561054645076a
Tayo Akosile <akosilety9@gmail.com>
2021-05-31 16:33:35 +0100

    Updated the fiiles

--- src/websocket/wb.js
+++ src/websocket/wb.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client'
 
-const socket = io('https://whatsapp-cloned-backend.herokuapp.com/', {
+const socket = io('https://whatsapp-cloned-backend.herokuapp.com', {
  withCredentials: true,
+ 
  extraHeaders: {
   'my-custom-header': 'abcd',
  },
